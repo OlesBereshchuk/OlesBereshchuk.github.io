a2f93a7b5643aabb391172a3fe509767eeb9abc4
OlesBereshchuk <Olesb@ukr.net>
2017-04-04 00:15:05 +0300

    Lama-m

---
+++ lama-m/js/main.js
@@ -0,0 +1,6 @@
+$('.sliderslick').slick({
+	autoplay: true,
+	vertical: true,
+	prevArrow: '<button type="button" class="slick-prev">Previous</button>' ,
+	nextArrow: '<button type="button" class="slick-next">Next</button>'
+});
